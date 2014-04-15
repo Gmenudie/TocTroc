e01446bb7e8ad2f0c59a4c4baa7d3f238b13a3d7
Victor <victor.enaud@hotmail.fr>
2014-04-15 18:43:01 +0200

    Ajout de l'image de profil redimensionnée, correction de bugs

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -95,8 +95,10 @@ class UsersController extends AppController {
 					
 					if($photo != $newphoto)
 					{
-						/* La photo de profil a changée, on modifie la base de données. (Si la photo de profil ne change pas, on ne fait rien) */
+						/* La photo de profil a changé, on modifie la base de données. (Si la photo de profil ne change pas, on ne fait rien) */
 						$go = 1;
+						/* On crée dès maintenant le chemin vers là où sera stocké l'image */
+						$cheminFichier = 'img/user/'.$id.'/';
 					}
 				}
 				else
@@ -111,21 +113,163 @@ class UsersController extends AppController {
 					/* On vérifie si le format du fichier est valide */
 					if(in_array($format, $formats))
 					{
-						/* On défini le nom du fichier PRENOM-NOM.FORMAT*/
-						$nom = $this->data['User']['prenom'].'-'.$this->data['User']['nom'].'.'.$format;
 						
-						/* On préfixe le nom du fichier */
-						if($this->User->findByUserId($id)['User']['image_profil'] != null || !isset($id))
-						{
-							$nom = 'profil-' . (substr($this->User->findByUserId($id)['User']['image_profil'], 7, 1) + 1) . '-' . $nom;
-						}
-						else
-						{
-							$nom = 'profil-1-' . $nom;
-						}
+						// /* On défini le nom du fichier PRENOM-NOM.FORMAT*/
+						// $nom = $this->data['User']['prenom'].'-'.$this->data['User']['nom'].'.'.$format;
 						
-						$cheminFichier = 'img/profil/'.$nom;
+						// /* On préfixe le nom du fichier */
+						// if($this->User->findByUserId($id)['User']['image_profil'] != null || !isset($id))
+						// {
+							// $nom = 'profil-' . (substr($this->User->findByUserId($id)['User']['image_profil'], 7, 1) + 1) . '-' . $nom;
+						// }
+						// else
+						// {
+							// $nom = 'profil-1-' . $nom;
+						//}
+						
+						
+						/* On va maintenant entamer le processus de redimmensionnement de l'image */
+						if($format == 'jpg' || $format == 'jpeg')
+						{
+							$imageChoisie = imagecreatefromjpeg($this->data['User']['upload_profil']['tmp_name']);
+							$tailleImageChoisie = getimagesize($this->data['User']['upload_profil']['tmp_name']);
+							
+							/* On définit les nouvelles dimensions de l'image */
+							$nouvelleLargeur = 80;
+							$nouvelleLargeurMiniature = 40;
+							
+							$reduction = ($nouvelleLargeur/$tailleImageChoisie[0]);
+							$reductionMiniature = ($nouvelleLargeurMiniature/$tailleImageChoisie[0]);
+							
+							$nouvelleHauteur = ($tailleImageChoisie[1] * $reduction);
+							$nouvelleHauteurMiniature = ($tailleImageChoisie[1] * $reductionMiniature);
+							
+							/* On crée l'image dans laquelle sera enregistrée l'image redimensionnée */
+							$nouvelleImage = imagecreatetruecolor($nouvelleLargeur, $nouvelleHauteur);
+							$nouvelleMiniature = imagecreatetruecolor($nouvelleLargeurMiniature, $nouvelleHauteurMiniature);
+							
+							/* On redimensionne l'image */
+							imagecopyresampled($nouvelleImage, $imageChoisie, 0, 0, 0, 0, $nouvelleLargeur, $nouvelleHauteur, $tailleImageChoisie[0], $tailleImageChoisie[1]);
+							imagecopyresampled($nouvelleMiniature, $imageChoisie, 0, 0, 0, 0, $nouvelleLargeurMiniature, $nouvelleHauteurMiniature, $tailleImageChoisie[0], $tailleImageChoisie[1]);
+							
+							/* On supprime la copie locale de l'image */
+							imagedestroy($imageChoisie);
+							
+							/* On upload et on regarde si ça se passe bien /!\ L'upload n'est possible que pour un utilisateur existant à ce stade, pour une création c'est plus loin /!\*/
+							if(isset($this->data['User']['user_id']))
+							{
+								if(imagejpeg($nouvelleImage, $cheminFichier.'profil.'.$format, 80) && imagejpeg($nouvelleMiniature, $cheminFichier.'miniature.'.$format, 80))
+								{
+									$upload = 1;
+								}
+								else
+								{
+									$this->Session->setFlash('Erreur lors de l\'upload de la photo de profil...','error');
+									$this->redirect($this->referer());
+								}
+							}
+							else
+							{
+								$goUpload = 1;
+							}
+						}
+						elseif($format == 'png')
+						{
+							$imageChoisie = imagecreatefrompng($this->data['User']['upload_profil']['tmp_name']);
+							$tailleImageChoisie = getimagesize($this->data['User']['upload_profil']['tmp_name']);
+							
+							/* On définit les nouvelles dimensions de l'image */
+							$nouvelleLargeur = 80;
+							$nouvelleLargeurMiniature = 40;
+							
+							$reduction = ($nouvelleLargeur/$tailleImageChoisie[0]);
+							$reductionMiniature = ($nouvelleLargeurMiniature/$tailleImageChoisie[0]);
+							
+							$nouvelleHauteur = ($tailleImageChoisie[1] * $reduction);
+							$nouvelleHauteurMiniature = ($tailleImageChoisie[1] * $reductionMiniature);
+							
+							/* On crée l'image dans laquelle sera enregistrée l'image redimensionnée */
+							$nouvelleImage = imagecreatetruecolor($nouvelleLargeur, $nouvelleHauteur);
+							$nouvelleMiniature = imagecreatetruecolor($nouvelleLargeurMiniature, $nouvelleHauteurMiniature);
+							
+							/* On redimensionne l'image */
+							imagecopyresampled($nouvelleImage, $imageChoisie, 0, 0, 0, 0, $nouvelleLargeur, $nouvelleHauteur, $tailleImageChoisie[0], $tailleImageChoisie[1]);
+							imagecopyresampled($nouvelleMiniature, $imageChoisie, 0, 0, 0, 0, $nouvelleLargeurMiniature, $nouvelleHauteurMiniature, $tailleImageChoisie[0], $tailleImageChoisie[1]);
+							
+							/* On supprime la copie locale de l'image */
+							imagedestroy($imageChoisie);
+							
+							/* On upload et on regarde si ça se passe bien /!\ L'upload n'est possible que pour un utilisateur existant à ce stade, pour une création c'est plus loin /!\*/
+							if(isset($this->data['User']['user_id']))
+							{
+								if(imagepng($nouvelleImage, $cheminFichier.'profil.png', 80) && imagepng($nouvelleMiniature, $cheminFichier.'miniature.png', 80))
+								{
+									$upload = 1;
+								}
+								else
+								{
+									$this->Session->setFlash('Erreur lors de l\'upload de la photo de profil...','error');
+									$this->redirect($this->referer());
+								}
+							}
+							else
+							{
+								$goUpload = 1;
+							}
+						}
+						elseif($format == 'gif')
+						{
+							$imageChoisie = imagecreatefromgif($this->data['User']['upload_profil']['tmp_name']);
+							$tailleImageChoisie = getimagesize($this->data['User']['upload_profil']['tmp_name']);
+							
+							/* On définit les nouvelles dimensions de l'image */
+							$nouvelleLargeur = 80;
+							$nouvelleLargeurMiniature = 40;
+							
+							$reduction = ($nouvelleLargeur/$tailleImageChoisie[0]);
+							$reductionMiniature = ($nouvelleLargeurMiniature/$tailleImageChoisie[0]);
+							
+							$nouvelleHauteur = ($tailleImageChoisie[1] * $reduction);
+							$nouvelleHauteurMiniature = ($tailleImageChoisie[1] * $reductionMiniature);
+							
+							/* On crée l'image dans laquelle sera enregistrée l'image redimensionnée */
+							$nouvelleImage = imagecreatetruecolor($nouvelleLargeur, $nouvelleHauteur);
+							$nouvelleMiniature = imagecreatetruecolor($nouvelleLargeurMiniature, $nouvelleHauteurMiniature);
+							
+							/* On joue avec l'alpha et l'omega pour garder la transparence des gifs */
+							imageAlphaBlending($nouvelleImage, false);
+							imageAlphaBlending($nouvelleMiniature, false);
+							imageSaveAlpha($nouvelleImage, true);
+							imageSaveAlpha($nouvelleMiniature, true);
+							
+							/* On redimensionne l'image */
+							imagecopyresampled($nouvelleImage, $imageChoisie, 0, 0, 0, 0, $nouvelleLargeur, $nouvelleHauteur, $tailleImageChoisie[0], $tailleImageChoisie[1]);
+							imagecopyresampled($nouvelleMiniature, $imageChoisie, 0, 0, 0, 0, $nouvelleLargeurMiniature, $nouvelleHauteurMiniature, $tailleImageChoisie[0], $tailleImageChoisie[1]);
+							
+							/* On supprime la copie locale de l'image */
+							imagedestroy($imageChoisie);
+							
+							/* On upload et on regarde si ça se passe bien /!\ L'upload n'est possible que pour un utilisateur existant à ce stade, pour une création c'est plus loin /!\*/
+							if(isset($this->data['User']['user_id']))
+							{
+								if(imagegif($nouvelleImage, $cheminFichier.'profil.gif', 80) && imagegif($nouvelleMiniature, $cheminFichier.'miniature.gif', 80))
+								{
+									$upload = 1;
+								}
+								else
+								{
+									$this->Session->setFlash('Erreur lors de l\'upload de la photo de profil...','error');
+									$this->redirect($this->referer());
+								}
+							}
+							else
+							{
+								$goUpload = 1;
+							}
+						}
+							
 						/* On regarde si l'upload s'est bien passé */
+						/*
 						if(move_uploaded_file($this->data['User']['upload_profil']['tmp_name'],$cheminFichier))
 						{
 							$upload = 1;
@@ -134,19 +278,12 @@ class UsersController extends AppController {
 						{
 							$this->Session->setFlash('Erreur lors de l\'upload de la photo de profil...','error');
 							$this->redirect($this->referer());
-						}
+						}*/
 					}
 					else
 					{
 						$this->Session->setFlash('Le format du fichier n\'est pas valide','error');
-						if(isset($this->data['User']['user_id']))
-						{
-							$this->redirect(array('controller' => 'users', 'action' => 'add', $id));
-						}
-						else
-						{
-							$this->redirect(array('controller' => 'users', 'action' => 'add'));
-						}
+						$this->redirect($this->referer());
 					}
 				}
 			}
@@ -161,9 +298,9 @@ class UsersController extends AppController {
 				{
 					/* L'email n'a pas changé */
 					$this->User->save($this->data);
-					if($upload=1)
+					if($upload==1)
 					{
-						$this->User->saveField('image_profil',$nom);
+						$this->User->saveField('image_profil',$format);
 					}
 					$this->Session->setFlash('Vos données ont été modifiées', 'success');
 					$this->redirect(array('controller' => 'users', 'action' => 'monCompte'));
@@ -176,9 +313,9 @@ class UsersController extends AppController {
 					{
 						/* L'email n'est pas déjà utilisé */
 						$this->User->save($this->data);
-						if($upload=1)
+						if($upload==1)
 						{
-							$this->User->saveField('image_profil',$nom);
+							$this->User->saveField('image_profil',$format);
 						}
 						$this->Session->setFlash('Vos données ont été modifiées', 'success');
 						$this->redirect(array('controller' => 'users', 'action' => 'monCompte'));
@@ -187,6 +324,7 @@ class UsersController extends AppController {
 					{
 					/* L'email est déjà pris */
 					$this->Session->setFlash('Cet email est déjà utilisé, désolé', 'error');
+					$this->redirect($this->referer());
 					}
 				}
 			}
@@ -205,15 +343,56 @@ class UsersController extends AppController {
 					$temporaryUser["User"]["role_id"]='3';
 				
 
-					if ($this->User->save($temporaryUser))
-					{   	
-						if($upload=1)
+					if ($id = $this->User->save($temporaryUser))
+					{   
+						/* On crée le dossier pour l'utilisateur */
+						new Folder('img/user/'.$id);
+					
+						if($goUpload==1)
 						{
-							$this->User->saveField('image_profil',$nom);
+							$cheminFichier = 'img/user/'.$id.'/';
+							$this->User->saveField('image_profil',$format);
+							
+							/* On upload et on regarde si ça se passe bien /!\ L'upload n'est possible que pour un utilisateur existant à ce stade, pour une création c'est plus loin /!\*/
+							if($format == 'jpg' || $format == 'jpeg')
+							{
+								if(imagejpeg($nouvelleImage, $cheminFichier.'profil.'.$format, 80) && imagejpeg($nouvelleMiniature, $cheminFichier.'miniature.'.$format, 80))
+								{
+									$upload = 1;
+								}
+								else
+								{
+									$this->Session->setFlash('Erreur lors de l\'upload de la photo de profil...','error');
+								}
+							}
+							elseif($format == 'png')
+							{
+								if(imagepng($nouvelleImage, $cheminFichier.'profil.png', 80) && imagepng($nouvelleMiniature, $cheminFichier.'miniature.png', 80))
+								{
+									$upload = 1;
+								}
+								else
+								{
+									$this->Session->setFlash('Erreur lors de l\'upload de la photo de profil...','error');
+								}
+							}
+							elseif($format == 'gif')
+							{
+								if(imagegif($nouvelleImage, $cheminFichier.'profil.gif', 80) && imagegif($nouvelleMiniature, $cheminFichier.'miniature.gif', 80))
+								{
+									$upload = 1;
+								}
+								else
+								{
+									$this->Session->setFlash('Erreur lors de l\'upload de la photo de profil...','error');
+								}
+							}
+							
 						}
 						
 						unset($temporaryUser);
 						$this->Session->setFlash(__('Bienvenue sur TocTroc !'));
+						$this->Auth->login($this->data);
 						return $this->redirect( array('controller' => 'acceuils', 'action' => 'index'));
 					}
 				}
@@ -222,6 +401,7 @@ class UsersController extends AppController {
 				{
 					/* L'email est déjà pris */
 					$this->Session->setFlash('Cet email est déjà utilisé, désolé', 'error');
+					$this->redirect($this->referer());
 				}
 			}
 			
@@ -234,7 +414,8 @@ class UsersController extends AppController {
 		}
 		else
 		{
-			$this->Session->setFlash(__('Erreur'));
+			$this->Session->setFlash(('Erreur'));
+			$this->redirect($this->referer());
 		}
 
     }
